5518b42b4801c871727275eb78d3b4b3e6ff1baa
AndesRafa <r.bustamante32@uniandes.edu.co>
2017-10-14 17:53:15 -0500

    Paso 4 - Verde: Se agregó código para soportar login de independiente

--- buscoayuda/urls.py
+++ buscoayuda/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+
     url(r'^register', views.register),
+
     url(r'^trabajador/(?P<pk>\d+)$', views.detail),
     url(r'^detail', views.detalle_trabajador),
+
+    url(r'^login$', views.login),
 ]
--- buscoayuda/views.py
+++ buscoayuda/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 from django.core import serializers
+from django.contrib import auth, messages
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponse
 
@@ -19,6 +20,11 @@ def index(request):
                'form_trabajador': form_trabajador,
                'form_usuario': form_usuario,
                'base_url': settings.STATIC_URL}
+
+    my_user = auth.get_user(request)
+
+    if my_user.is_authenticated:
+        context['user_name'] = trabajadores.filter(usuarioId=my_user).first().nombre
     # context = {'trabajadores': trabajadores, 'tipos_de_servicios': tipos_de_servicios,
     #            'form_trabajador': form_trabajador, 'form_usuario': form_usuario, 'base_url': settings.STATIC_URL}
     return render(request, 'buscoayuda/index.html', context)
@@ -50,6 +56,7 @@ def register(request):
 
     return HttpResponseRedirect('/')
 
+
 def detalle_trabajador(request):
     return render(request, "buscoayuda/detalle.html")
 
@@ -57,3 +64,18 @@ def detalle_trabajador(request):
 def detail(request, pk):
     trabajador = get_object_or_404(Trabajador, pk=pk)
     return HttpResponse(serializers.serialize("json", [trabajador]))
+
+
+def login(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+    if user is not None:
+        auth.login(request, user)
+        messages.success(request, "Bienvenido al sistema {}".format(
+            username), extra_tags="alert-success")
+        return HttpResponseRedirect('/')
+    else:
+        messages.error(
+            request, "¡El usuario o la contraseña son incorrectos!", extra_tags="alert-danger")
+        return HttpResponseRedirect('/')
