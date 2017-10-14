9aa979136d323c919f839e0d8985baaa6f42a30e
AndesRafa <r.bustamante32@uniandes.edu.co>
2017-10-14 08:54:04 -0500

    Paso 1 - Rojo - Se agregó la prueba para el titulo de la pagina de indice

--- buscoayuda/tests.py
+++ buscoayuda/tests.py
@@ -1,3 +1,19 @@
+#from unittest import TestCase
+
 from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Create your tests here.
+class Kata_TDD_Test(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(2)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_title(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Busco Ayuda', self.browser.title)
